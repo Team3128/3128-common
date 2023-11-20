a9d6427d81a97ec8118191d65a11a11bf921df0d
Mason-Lam <97353903+Mason-Lam@users.noreply.github.com>
2023-11-19 21:57:26 -0800

    Narwhal Dashboard comments

--- src/main/java/common/narwhaldashboard/NarwhalDashboard.java
+++ src/main/java/common/narwhaldashboard/NarwhalDashboard.java
@@ -18,6 +18,11 @@ import org.json.simple.JSONObject;
 import common.utility.Log;
 import edu.wpi.first.util.function.BooleanConsumer;
 
+/**
+ * Team 3128's {@link WebSocketServer} class, used to log robot data and select autos
+ * @since Deep Space 2019
+ * @author Mason Lam
+ */
 public class NarwhalDashboard extends WebSocketServer {
     private final HashMap<String, Supplier<Object>> updateMap = new HashMap<String, Supplier<Object>>();
     private final HashMap<String, List<Object>> initMap = new HashMap<String, List<Object>>();
@@ -41,10 +46,6 @@ public class NarwhalDashboard extends WebSocketServer {
         return instance;
     }
 
-    public static void setPort(int port) {
-        PORT = port;
-    }
-
     /**
      * Starts the NarwhalDashboard server. This opens it up to be able to be
      * connected to by client devices (the DS Laptop, a tablet controller, etc) and
@@ -63,27 +64,53 @@ public class NarwhalDashboard extends WebSocketServer {
         }
     }
 
+    /**
+     * Sets the IP address' port number
+     * @param port the port number
+     */
+    public static void setPort(int port) {
+        PORT = port;
+    }
+
+    /**
+     * Creates a new NarwhalDashboard server
+     * @param port the port number
+     * @throws UnknownHostException invalid IP address
+     */
     private NarwhalDashboard(int port) throws UnknownHostException {
         super(new InetSocketAddress(port));
     }
 
+    /**
+     * Initializes NarwhalDashboard autos and buttons
+     */
     private void initDashboard() {
         addUpdate("selectedAuto", ()-> selectedAuto);
         addAction("selectAuto", autoName -> selectAuto(autoName[0]));
         addAction("button", button -> updateButton(button[0], button[1].equals("down")));
     }
 
+    /**
+     * Sends a list of objects to the web server on intialize
+     * @param key Name of the objects on dashboard
+     * @param objects List of objects added
+     */
     public void addInit(String key, List<Object> objects) {
         initMap.put(key, objects);
     }
 
+    /**
+     * Sends an object to the web server on intialize
+     * @param key Name of the object on dashboard
+     * @param object - Object added
+     */
     public void addInit(String key, Object object) {
         addInit(key, Arrays.asList(object));
     }
     /**
      * Adds an autonomous program to NarwhalDashboard's auto picker
      * 
-     * @param names    - The human-readable name of the autonomous program
+     * @param names - The human-readable name of the autonomous program
      */
     @SuppressWarnings("all")
     public void addAutos(String... names) {
@@ -91,7 +118,11 @@ public class NarwhalDashboard extends WebSocketServer {
         addInit("auto", Arrays.asList(names));
     }
 
-    // Called once on connection with web server
+    /** 
+     * Called after connecting to the web server
+	 * @param conn The <tt>WebSocket</tt> instance this event is occuring on.
+	 * @param handshake The handshake of the websocket instance
+	 */
     @SuppressWarnings("unchecked")
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
@@ -100,13 +131,16 @@ public class NarwhalDashboard extends WebSocketServer {
         this.conn = conn;
 
         final JSONObject obj = new JSONObject();
+        //Sends every object to NarwhalDashboard on initialize
         for (final String key : initMap.keySet()) {
             final List<Object> values = initMap.get(key);
+            //Sends a singular object
             if (values.size() <= 1) {
                 obj.put(key, values.get(0));
                 continue;
             }
 
+            //Sends a list of objects
             final JSONArray arr = new JSONArray();
             for (final Object value : values) {
                 arr.add(value);
@@ -117,11 +151,24 @@ public class NarwhalDashboard extends WebSocketServer {
         conn.send(obj.toJSONString());
     }
 
+    /**
+     * Sends an object to the web server every update
+     * @param key Name of the object
+     * @param obj Object added
+     */
+    public void addUpdate(String key, Supplier<Object> obj) {
+        updateMap.put(key, obj);
+    }
+
+    /**
+     * Updates NarwhalDashboard sending data to the web server
+     */
     @SuppressWarnings("unchecked")
     public void update() {
         if (conn == null) return;
 
         if (conn.isOpen()) {
+            //Sends data as a JSON
             final JSONObject obj = new JSONObject();
             for (final String key : updateMap.keySet()) {
                 obj.put(key, updateMap.get(key).get());
@@ -130,18 +177,45 @@ public class NarwhalDashboard extends WebSocketServer {
         }
     }
 
-    public void addUpdate(String key, Supplier<Object> obj) {
-        updateMap.put(key, obj);
+    /**
+     * Adds an action to be run on a message from the web server
+     * @param key Name of the action
+     * @param action - Action to be run
+     */
+    public void addAction(String key, Consumer<String[]> action) {
+        actionMap.put(key, action);
     }
 
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-    }
-
+    /**
+     * Assigns an action to a NarwhalDashboard button
+     * @param key Name of the button
+     * @param button Action to be run on button press
+     */
     public void addButton(String key, BooleanConsumer button) {
         buttons.put(key, button);
-    } 
+    }
 
+    /**
+	 * Called when a message from the web server is sent
+	 * @param conn The <tt>WebSocket</tt> instance this event is occurring on.
+	 * @param message The UTF-8 decoded message that was received.
+	 */
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        Log.info("NarwhalDashboard", message);
+        final String[] parts = message.split(":");
+
+        if (!actionMap.containsKey(parts[0])) return;
+
+        final Consumer<String[]> action = actionMap.get(parts[0]);
+
+        action.accept(Arrays.copyOfRange(parts, 1, parts.length));
+    }
+
+    /**
+     * Selects the auto
+     * @param autoName Name of the auto
+     */
     private void selectAuto(String autoName) {
         if (autoName == null) {
             selectedAuto = null;
@@ -156,29 +230,17 @@ public class NarwhalDashboard extends WebSocketServer {
         Log.recoverable("NarwhalDashboard", "Auto program \"" + autoName + "\" does not exist.");
     }
 
+    /**
+     * Changes a button state
+     * @param key Name of the button
+     * @param down State of the button
+     */
     private void updateButton(String key, boolean down) {
         if (!buttons.containsKey(key)) {
             Log.recoverable("NarwhalDashboard", "Button \"" + key + "\" was never added.");
             return;
         }
         buttons.get(key).accept(down);
-    }
-
-    public void addAction(String key, Consumer<String[]> action) {
-        actionMap.put(key, action);
-    }
-
-    // Called by request from web server 
-    @Override
-    public void onMessage(WebSocket conn, String message) {
-        Log.info("NarwhalDashboard", message);
-        final String[] parts = message.split(":");
-
-        if (!actionMap.containsKey(parts[0])) return;
-
-        final Consumer<String[]> action = actionMap.get(parts[0]);
-
-        action.accept(Arrays.copyOfRange(parts, 1, parts.length));
     }
 
     @Override
@@ -188,5 +250,8 @@ public class NarwhalDashboard extends WebSocketServer {
 
     @Override
     public void onStart() {}
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {}
 
 }
