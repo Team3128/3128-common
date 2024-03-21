097496aef21932f6900b5ba5d53c9518e1b2f731
Spooky1000 <teja.yaramada@gmail.com>
2024-03-20 17:09:11 -0700

    attempting to implement common in module
    this code does not build. one possible solution could be to have the processor generate code that contains the update within it that is then called by narwhal dashboard.

--- processorModule/src/main/java/common/CommonProcessor.java
+++ processorModule/src/main/java/common/CommonProcessor.java
@@ -1,8 +1,11 @@
 // package common.utility.narwhaldashboard;
 package common;
 
+// import src.main.java.common.utility.narwhaldashboard.NarwhalDashboard;
+
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -17,6 +20,7 @@ import com.google.auto.service.AutoService;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.Set;
 
@@ -35,24 +39,83 @@ public class CommonProcessor extends AbstractProcessor{
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Annotation Processor Running...");
-        annotations.forEach(annotation ->
+        try{
+            annotations.forEach(annotation ->
             roundEnv.getElementsAnnotatedWith(annotation).forEach(
-                element -> generateFile((Element) element)));
+                element -> processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, element.getSimpleName().toString() + " has been processed")));
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'process'");
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
-    private void generateFile(Element element) {
-        String className = element.getSimpleName().toString() + "Generated";
-        try {
-            JavaFileObject file = processingEnv.getFiler().createSourceFile(className);
-            try (Writer writer = file.openWriter()) {
-                writer.write("public class " + className + " {\n" +
-                        "    // This is a generated file for element: " + element.getSimpleName() + "\n" +
-                        "}\n");
+    private void writeBuilderFile(String className, Map<String, String> setterMap) throws IOException {
+        String packageName = null;
+        int lastDot = className.lastIndexOf('.');
+        if (lastDot > 0) {
+            packageName = className.substring(0, lastDot);
+        }
+
+        String simpleClassName = className.substring(lastDot + 1);
+        String builderClassName = className + "Builder";
+        String builderSimpleClassName = builderClassName
+        .substring(lastDot + 1);
+
+        JavaFileObject builderFile = processingEnv.getFiler()
+        .createSourceFile(builderClassName);
+        
+        try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
+
+            if (packageName != null) {
+                out.print("package ");
+                out.print(packageName);
+                out.println(";");
+                out.println();
             }
-        } catch (IOException e) {
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "An error occurred: " + e.getMessage());
+
+            out.print("public class ");
+            out.print(builderSimpleClassName);
+            out.println(" {");
+            out.println();
+
+            out.print("    private ");
+            out.print(simpleClassName);
+            out.print(" object = new ");
+            out.print(simpleClassName);
+            out.println("();");
+            out.println();
+
+            out.print("    public ");
+            out.print(simpleClassName);
+            out.println(" build() {");
+            out.println("        return object;");
+            out.println("    }");
+            out.println();
+
+            setterMap.entrySet().forEach(setter -> {
+                String methodName = setter.getKey();
+                String argumentType = setter.getValue();
+
+                out.print("    public ");
+                out.print(builderSimpleClassName);
+                out.print(" ");
+                out.print(methodName);
+
+                out.print("(");
+
+                out.print(argumentType);
+                out.println(" value) {");
+                out.print("        object.");
+                out.print(methodName);
+                out.println("(value);");
+                out.println("        return this;");
+                out.println("    }");
+                out.println();
+            });
+
+            out.println("}");
         }
     }
 
