4fe218ea47da3c11c76a9421a09359aed0f468d3
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-03-26 16:44:39 -0700

    Prepare for release

--- src/main/java/common/core/misc/NAR_Robot.java
+++ src/main/java/common/core/misc/NAR_Robot.java
@@ -4,16 +4,13 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 
 import org.littletonrobotics.junction.AutoLogOutputManager;
-import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
-import org.reflections.Reflections;
 
 import edu.wpi.first.hal.DriverStationJNI;
 import edu.wpi.first.hal.HAL;
@@ -22,7 +19,6 @@ import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobotBase;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
@@ -95,8 +91,6 @@ public class NAR_Robot extends IterativeRobotBase {
 
     private final GcStatsCollector gcStatsCollector = new GcStatsCollector();
 
-    private static List<Method> processorGeneratedContainers = new LinkedList<Method>();
-
     /** Constructor for TimedRobot. */
     protected NAR_Robot() {
         this(kDefaultPeriod);
@@ -149,33 +143,11 @@ public class NAR_Robot extends IterativeRobotBase {
         NotifierJNI.cleanNotifier(m_notifier);
     }
 
-    public static void addProcessorGeneratedContainer(String classPath) {
-        try {
-            Class<?> container = Class.forName(classPath);
-            Method updateMethod = container.getDeclaredMethod("update");
-            processorGeneratedContainers.add(updateMethod);
-        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException e) {
-            System.out.println("Failed to add processor generated container: " + classPath);
-            e.printStackTrace();
-        }
-    }
-
-    private void containersInit() {
-        for(Method updateMethod : processorGeneratedContainers) {
-            try {
-                updateMethod.invoke(null);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     /** Provide an alternate "main loop" via startCompetition(). */
     @Override
     public void startCompetition() {
         long initStart = Logger.getRealTimestamp();
         robotInit();
-        containersInit();
 
         if (isSimulation()) {
             simulationInit();
