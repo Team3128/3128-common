c4c014c144195927a4983704c5c2e1ae40a36b77
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2025-01-05 15:59:18 -0800

    prepare for release

--- src/main/java/common/hardware/motorcontroller/NAR_Motor.java
+++ src/main/java/common/hardware/motorcontroller/NAR_Motor.java
@@ -96,33 +96,8 @@ public abstract class NAR_Motor implements AutoCloseable {
     protected double unitConversionFactor = 1;
     protected double timeConversionFactor = 1;
 
-    @AutoLog
-	public static class NAR_MotorIO{ 
-        public double inputPower = 0;
-        public double appliedOutput = 0;
-        public double stallCurrent = 0;
-        public double position;
-        public double velocity = 0;
-	}
-
-    private NAR_MotorIOAutoLogged io;
-
-    public void updateIO(NAR_MotorIOAutoLogged io){
-        io.inputPower = prevValue;
-        io.appliedOutput = getAppliedOutput();
-        io.stallCurrent = getStallCurrent();
-        io.position = getPosition();
-        io.velocity = getVelocity();
-    }
-
     public NAR_Motor(int id){
-        if (NAR_Robot.logWithAdvantageKit) {
-            io = new NAR_MotorIOAutoLogged();
-            NAR_Robot.addPeriodic(()-> {
-                updateIO(io);
-                Logger.processInputs("Motors/" + id, io);
-            }, 0.02);
-        }
+        
     }
 
     /**
