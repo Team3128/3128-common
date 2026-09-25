04387d110d929bdc5d18a4505b7393d3a5be59f4
ThatTMan <tpdimmock@gmail.com>
2026-09-25 15:54:20 -0700

    common stuff state spaces

--- src/main/java/common/core/controllers/VelocitySSController.java
+++ src/main/java/common/core/controllers/VelocitySSController.java
@@ -2,6 +2,7 @@ package common.core.controllers;
 import edu.wpi.first.math.VecBuilder;
 import common.hardware.motorcontroller.NAR_Motor;
 import common.utility.Log;
+import common.utility.shuffleboard.NAR_Shuffleboard;
 import edu.wpi.first.math.controller.LinearQuadraticRegulator;
 import edu.wpi.first.math.estimator.KalmanFilter;
 import edu.wpi.first.math.numbers.N1;
@@ -9,6 +10,7 @@ import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.LinearSystemLoop;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.Num;
@@ -18,38 +20,47 @@ public class VelocitySSController<N extends Num> extends ControllerBase {
     public NAR_Motor motor;
     public LinearSystemLoop<N1, N1, N1> loop;
     public double setpoint;
+    public KalmanFilter<N1, N1, N1> observer;
     
     public VelocitySSController(PIDFFConfig config, Vector<N1> stateSTD, Vector<N1> measurementSTD, Vector<N1> qelms, Vector<N1> relms, double tolerance) {
         super(config, tolerance);
         
         LinearSystem<N1, N1, N1> system = LinearSystemId.identifyVelocitySystem(config.getkV(), config.getkA());
-        KalmanFilter<N1, N1, N1> observer = new KalmanFilter<N1, N1, N1>(Nat.N1(), Nat.N1(), system, stateSTD, measurementSTD, 0.020);
+        this.observer = new KalmanFilter<N1, N1, N1>(Nat.N1(), Nat.N1(), system, stateSTD, measurementSTD, 0.020);
         LinearQuadraticRegulator<N1, N1, N1> controller = new LinearQuadraticRegulator<N1, N1, N1>(system, qelms, relms, 0.020);
-        this.loop = new LinearSystemLoop<>(system, controller, observer, 12.0, 0.020);
+        this.loop = new LinearSystemLoop<>(system, controller, this.observer, 12.0, 0.020);
     }
+
+    public KalmanFilter<N1,N1,N1> getObserver() {
+        return this.observer;
+    }
+
+
 
     @Override
     public void setSetpoint(double setpoint) {
-        Log.info("Test", setpoint);
-        Log.info("AtSetpoint", Boolean.toString(atSetpoint()));
+        //Log.info("Test", setpoint);
+        //Log.info("AtSetpoint", Boolean.toString(atSetpoint()));
         this.setpoint = setpoint;
         this.loop.setNextR(VecBuilder.fill(setpoint));
+        Log.info("Checking", this.loop.getNextR(0));
     }
 
     @Override
     public void useOutput() {
         if (isEnabled() && atSetpoint()) disable();
+        if (!atSetpoint()) super.enabled = true;
 
         if (isEnabled()) {
         this.loop.correct(VecBuilder.fill(getMeasurement()));
         this.loop.predict(0.020);
         final double output = MathUtil.clamp(this.loop.getU(0), -1, 1);
         for (NAR_Motor motor : getMotors()) {
-            Log.info("AtSetpoint", Boolean.toString(atSetpoint()));
+            Log.info("skibidi", getMeasurement());
             Log.info("Test" , output);
             motor.setVolts(output);
         }} else {
-            Log.info("AtSetpoint", Boolean.toString(atSetpoint()));
+            //Log.info("AtSetpoint", Boolean.toString(atSetpoint()));
             reset();
         }
     }
@@ -66,15 +77,15 @@ public class VelocitySSController<N extends Num> extends ControllerBase {
 
     @Override
     public double getMeasurement() {
-        Log.info("Velocity", motor.getVelocity());
+        //Log.info("Velocity", motor.getVelocity());
         return motor.getVelocity();
     }
 
     @Override
     public boolean atSetpoint() {
         
-        Log.info("current", getMeasurement());
-        Log.info("target", getSetpoint());
+        //Log.info("current", getMeasurement());
+        //Log.info("target", getSetpoint());
         return Math.pow(getMeasurement() - getSetpoint(), 2) < Math.pow(tolerance, 2);
     }
 
